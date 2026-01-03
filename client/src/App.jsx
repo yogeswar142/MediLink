1279aa1bf8f93e7111d980a80204abf5fc6eb7c6
yogeswar142 <amarayogeswararao16@gmail.com>
2026-01-03 13:34:04 +0530

    Setup React + Tailwind CSS (working)

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-3xl">
+      Tailwind working ✅
+    </div>
+  );
+}
+
+export default App;
+
