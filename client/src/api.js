ee38e637725b56940c0285a2653dd618c0d30336
yogeswar142 <amarayogeswararao16@gmail.com>
2026-03-12 21:31:58 +0530

    API Update

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://medilink-api.ddns.net";
 export default API_BASE_URL;
